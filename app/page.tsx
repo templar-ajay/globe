2bb3b3a37cc250aeae426c1c8e643ba8dc3449be
templar-ajay <templar.ajay@gmail.com>
2025-01-17 01:30:59 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { GlobeDemo } from "@/components/my-comps/my-globe";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full min-h-screen">
+      <GlobeDemo />
+    </main>
+  );
+}
---
+++ components/my-comps/my-globe.tsx
@@ -0,0 +1,20 @@
+import Globe from "@/components/ui/globe";
+import { ThemeModeToggle } from "../theme-toggle";
+
+export function GlobeDemo() {
+  return (
+    <div className="relative min-h-screen flex flex-col size-full max-w-full items-center justify-start overflow-hidden rounded-lg border bg-background px-40 pb-40 pt-8 md:pb-60 md:shadow-xl">
+      <div className="absolute right-2 sm:right-4 top-2 sm:top-4">
+        <ThemeModeToggle />
+      </div>
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b dark:bg-gradient-to-t from-black to-to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+        Globe
+      </span>
+
+      <Globe className="top-20 max-w-full" />
+      <div
+        className={`pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]`}
+      />
+    </div>
+  );
+}
